48cb2f1a75ac9fcf26d8175b3632f4dc62ec016e
jykim <wodusl1103@naver.com>
2022-06-21 18:44:29 +0900

    feat: 기본 구분자(',', ':')로 문자열 분리

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,8 +1,29 @@
 package calculator;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class StringAddCalculator {
+
+    private static final String BASE_DIVIDER_COMMA = ",";
+    private static final String BASE_DIVIDER_COLON = ":";
 
     public static int splitAndSum(String input) {
         return 0;
+    }
+
+    public static String[] splitByDivider(String input) {
+        String[] result = new String[] {};
+        String[] splitByComma = splitByDivider(input, BASE_DIVIDER_COMMA);
+        for (String element : splitByComma) {
+            result = Stream
+                .concat(Arrays.stream(result), Arrays.stream(splitByDivider(element, BASE_DIVIDER_COLON)))
+                .toArray(String[]::new);
+        }
+        return result;
+    }
+
+    public static String[] splitByDivider(String input, String divider) {
+        return input.split(divider);
     }
 }
---
+++ src/test/java/calculator/StringAddCalculatorTest_Base.java
@@ -0,0 +1,14 @@
+package calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringAddCalculatorTest_Base {
+
+    @Test
+    void 문자열_구분_쉼표_콜론() {
+        String[] result = StringAddCalculator.splitByDivider("1,2:3");
+        assertThat(result).containsExactly("1", "2", "3");
+    }
+}
